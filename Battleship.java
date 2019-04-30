6ec88281dfc550ca7fa1ff9d0257b0049e34ea6d
Zhijun Sheng <zhijunsheng@gmail.com>
2019-04-30 19:28:19 -0400

    implemented empty board

--- Battleship.java
+++ Battleship.java
@@ -2,6 +2,23 @@ class Battleship {
 
   public static void main(String[] args) {
     System.out.println("Hello Battleship");
+    BattleshipBoard brd = new BattleshipBoard();
+    System.out.println(brd);
   }
 }
 
+class BattleshipBoard {
+  final static int ROWS = 10;
+  final static int COLS = 10;
+  
+  public String toString() {
+    String brdStr = "";
+    for (int row = 0; row < ROWS; row++) {
+      for (int col = 0; col < COLS; col++) {
+        brdStr += " .";
+      }
+      brdStr += "\n";
+    }
+    return brdStr;
+  }
+}
