ffbe43db36a9cf2b55f1e0adc0a2fe08f17c315a
Zhijun Sheng <zhijunsheng@gmail.com>
2019-04-30 20:59:10 -0400

    added 3 non-random ships

--- Battleship.java
+++ Battleship.java
@@ -10,12 +10,65 @@ class Battleship {
 class BattleshipBoard {
   final static int ROWS = 10;
   final static int COLS = 10;
+
+  private int[][] carrier = {
+    {0, 0}, // col, row
+    {0, 1},
+    {0, 2},
+    {0, 3},
+  };
   
+  private int[][] submarine = {
+    {2, 2}, // col, row
+    {3, 2},
+    {4, 2},
+  };
+
+  private int[][] battleship = {
+    {2, 4}, // col, row
+    {3, 4},
+  };
+
+  private boolean isCarrier(int col, int row) {
+    for (int cellIndex = 0; cellIndex < carrier.length; cellIndex++) {
+      if (col == carrier[cellIndex][0] && row == carrier[cellIndex][1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isSubmarine(int col, int row) {
+    for (int cellIndex = 0; cellIndex < submarine.length; cellIndex++) {
+      if (col == submarine[cellIndex][0] && row == submarine[cellIndex][1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isBattleship(int col, int row) {
+    for (int cellIndex = 0; cellIndex < battleship.length; cellIndex++) {
+      if (col == battleship[cellIndex][0] && row == battleship[cellIndex][1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   public String toString() {
     String brdStr = "";
     for (int row = 0; row < ROWS; row++) {
       for (int col = 0; col < COLS; col++) {
-        brdStr += " .";
+        if (isCarrier(col, row)) {
+          brdStr += " c";
+        } else if (isSubmarine(col, row)) {
+          brdStr += " s";
+        } else if (isBattleship(col, row)) {
+          brdStr += " b";
+        } else {
+          brdStr += " .";
+        }
       }
       brdStr += "\n";
     }
